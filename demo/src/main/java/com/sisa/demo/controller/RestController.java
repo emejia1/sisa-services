dbbe53c7ee3442f2c13002f086211f4652648a33
emejia <emejia@sisa.com>
2022-08-20 18:28:16 -0600

    se ha modificado las dependencias para que funcione con java 16

--- demo/src/main/java/com/sisa/demo/controller/RestController.java
+++ demo/src/main/java/com/sisa/demo/controller/RestController.java
@@ -1,13 +1,18 @@
 package com.sisa.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.swagger.annotations.Api;
+
+@Api(tags = "SISA API")
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/sisa")
 public class RestController {
 
 	@GetMapping
-	public String saludo(String nombre) {
-		return "Hola " + nombre;
+	public String saludo() {
+		return "Hola Mundo SISA";
 	}
 
 }
